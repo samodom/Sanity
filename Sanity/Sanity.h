a80073c416bddfa55ffc64cdf4dd7b3ca69b44be
Sam Odom <samodom@me.com>
2014-06-28 12:40:34 -0500

    Initial Commit

---
+++ Sanity/Sanity.h
@@ -0,0 +1,19 @@
+//
+//  Sanity.h
+//  Sanity
+//
+//  Created by Sam Odom on 6/28/14.
+//  Copyright (c) 2014 Sam Odom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sanity.
+FOUNDATION_EXPORT double SanityVersionNumber;
+
+//! Project version string for Sanity.
+FOUNDATION_EXPORT const unsigned char SanityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sanity/PublicHeader.h>
+
+
