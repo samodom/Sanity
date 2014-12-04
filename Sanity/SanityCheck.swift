a10b162500fc1e386736b5d114f379cd8b08eeb9
Sam Odom <samodom@me.com>
2014-12-03 22:23:21 -0600

    Added initial subclass declaration

---
+++ Sanity/SanityCheck.swift
@@ -0,0 +1,13 @@
+//
+//  SanityCheck.swift
+//  Sanity
+//
+//  Created by Sam Odom on 12/3/14.
+//  Copyright (c) 2014 Sam Odom. All rights reserved.
+//
+
+import XCTest
+
+public class SanityCheck: XCTestCase {
+
+}
