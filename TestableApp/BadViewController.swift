a6f4ae206cfce8a29ed62fdb6c3785c74f710dd7
Sam Odom <samodom@me.com>
2015-03-15 13:56:33 -0500

    Added test view controllers

---
+++ TestableApp/BadViewController.swift
@@ -0,0 +1,69 @@
+//
+//  BadViewController.swift
+//  Sanity
+//
+//  Created by Sam Odom on 3/15/15.
+//  Copyright (c) 2015 Sam Odom. All rights reserved.
+//
+
+import UIKit
+
+class BadViewController: UIViewController {
+
+    internal override func loadView() {
+        super.loadView()
+    }
+
+    internal override func viewDidLoad() {
+
+    }
+
+    internal override func viewWillAppear(animated: Bool) {
+
+    }
+
+    internal override func viewDidAppear(animated: Bool) {
+
+    }
+
+    internal override func viewWillDisappear(animated: Bool) {
+
+    }
+
+    internal override func viewDidDisappear(animated: Bool) {
+
+    }
+
+    internal override func didReceiveMemoryWarning() {
+
+    }
+
+    internal override func updateViewConstraints() {
+
+    }
+
+    internal override func addChildViewController(childController: UIKit.UIViewController) {
+
+    }
+
+    internal override func removeFromParentViewController() {
+
+    }
+
+    internal override func transitionFromViewController(fromViewController: UIKit.UIViewController, toViewController: UIKit.UIViewController, duration: NSTimeInterval, options: UIViewAnimationOptions, animations: (() -> Void)?, completion: ((Bool) -> Void)?) {
+
+    }
+
+    internal override func setEditing(editing: Bool, animated: Bool) {
+
+    }
+
+    internal override func encodeRestorableStateWithCoder(coder: NSCoder) {
+        
+    }
+    
+    internal override func decodeRestorableStateWithCoder(coder: NSCoder) {
+        
+    }
+
+}
